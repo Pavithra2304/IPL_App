b942a6d0b2a5fd9a8cdf5d8db08e296d13614c99
Pavithra2304 <92969170+Pavithra2304@users.noreply.github.com>
2021-11-19 16:21:25 +0530

    Add files via upload

---
+++ IPL_App/IPL_App/PlayerBO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPL_App
+{
+   
+        class PlayerBO
+        {
+            public Player[] SearchBYTeamName(Player[] playerList)
+            {
+                //Player[] results=null;
+                SortingTeam s1 = new SortingTeam();
+                Array.Sort(playerList, s1);
+                //results = playerList;
+                return playerList;
+            }
+            public Player[] SearchBYBattingStyle(Player[] playerList)
+            {
+               Player[] results = null;
+                SortingPlayers sp = new SortingPlayers();
+                Array.Sort(playerList, sp);
+                Array.Reverse(playerList);
+                results = playerList;
+                return results;
+            }
+        }
+    
+}
---
+++ IPL_App/IPL_App/SortingPlayers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPL_App
+{
+    class SortingPlayers
+    {
+        class SortDesPlayer : IComparer<Player>
+        {
+            public int Compare(Player x, Player y)
+            {
+                return x.BattingStyle.CompareTo(y.BattingStyle);
+            }
+        }
+    }
+}
